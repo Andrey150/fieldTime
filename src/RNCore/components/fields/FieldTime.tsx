43b2fc05ba183b0833eee978e3b18cf8865bf6c0
Andrey150 <ananevandrey150@gmail.com>
2023-08-08 19:17:34 +0300

    remove duplication

--- src/RNCore/components/fields/FieldTime.tsx
+++ src/RNCore/components/fields/FieldTime.tsx
@@ -5,72 +5,80 @@ import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons
 import tw from "../../libs/tailwind";
 
 interface ITimeField {
-    class?: string // tailwind classes
-    value: string | null // Если не задано отображаем --:--
-    onChange?: (time: string) => void // событие должно вызываться при снятии фокуса с компонента.
-    useButtons?: boolean // если true - отображаем кнопки "<" слева и справа ">" + или - 1 час соответственно
+  class?: string // tailwind classes
+  value: string | null // Если не задано отображаем --:--
+  onChange?: (time: string) => void // событие должно вызываться при снятии фокуса с компонента.
+  useButtons?: boolean // если true - отображаем кнопки "<" слева и справа ">" + или - 1 час соответственно
 }
 
 export function FieldTime(props: ITimeField) {
-    const [time, setTime] = useState<string | null>(props.value);
+  const [time, setTime] = useState<string | null>(props.value);
+  const commonStyles = tw`text-xl text-center`;
 
-    const handleBlur = () => {
-        if (props.onChange) {
-            props.onChange(time || "");
-        }
-    };
+  const handleBlur = () => {
+    if (props.onChange) {
+      props.onChange(time || "");
+    }
+  };
 
-    const handleTimeChange = (newTime: string) => {
-        setTime(newTime);
-    };
+  const handleTimeChange = (newTime: string) => {
+    setTime(newTime);
+  };
 
-    const incrementHour = () => {
-        if (time) {
-            const [ hours, minutes] = time.split(":");
-            const newHours = parseInt(hours, 10) + 1;
-            const newTime = `${newHours.toString().padStart(2, "0")}:${minutes}`;
-            setTime(newTime);
-        }
-    };
+  const updateTime = (operation: string) => {
+    if (time) {
+      const [ hours, minutes] = time.split(":");
+      const newHours = parseInt(hours, 10) + parseInt(operation, 10);
+      const newTime = `${newHours.toString().padStart(2, "0")}:${minutes}`;
+      setTime(newTime);
+    }
+  }
 
-    const decrementHour = () => {
-        if (time) {
-            const [ hours, minutes] = time.split(":");
-            const newHours = parseInt(hours, 10) - 1;
-            const newTime = `${newHours.toString().padStart(2, "0")}:${minutes}`;
-            setTime(newTime);
-        }
-    };
+  const incrementHour = () => {
+    updateTime('1')
+  };
 
-    return (
-      <View style={tw`${props.class || ""}`}>
-          {props.useButtons && (
-            <View style={tw`flex-row items-center justify-center`}>
-                <TouchableOpacity onPress={decrementHour}>
-                    <FontAwesomeIcon icon={faChevronLeft} size={20} />
-                </TouchableOpacity>
-                <TextInput
-                  style={tw`text-xl text-center`}
-                  value={time || ""}
-                  onChangeText={handleTimeChange}
-                  onBlur={handleBlur}
-                  keyboardType="numeric"
-                />
-                <TouchableOpacity onPress={incrementHour}>
-                    <FontAwesomeIcon icon={faChevronRight} size={20} />
-                </TouchableOpacity>
-            </View>
-          )}
-          {!props.useButtons && (
-            <TextInput
-              style={tw`text-xl text-center`}
-              value={time || ""}
-              onChangeText={handleTimeChange}
-              onBlur={handleBlur}
-              keyboardType="numeric"
-            />
-          )}
-      </View>
-    );
+  const decrementHour = () => {
+    updateTime('-1')
+  };
+
+  const decrementButton = (
+    <TouchableOpacity onPress={decrementHour}>
+      <FontAwesomeIcon icon={faChevronLeft} size={20} />
+    </TouchableOpacity>
+  );
+
+  const incrementButton = (
+    <TouchableOpacity onPress={incrementHour}>
+      <FontAwesomeIcon icon={faChevronRight} size={20} />
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={tw`${props.class || ""}`}>
+      {props.useButtons && (
+        <View style={tw`flex-row items-center justify-center`}>
+          {decrementButton}
+          <TextInput
+            style={commonStyles}
+            value={time || ""}
+            onChangeText={handleTimeChange}
+            onBlur={handleBlur}
+            keyboardType="numeric"
+          />
+          {incrementButton}
+        </View>
+      )}
+      {!props.useButtons && (
+        <TextInput
+          style={commonStyles}
+          value={time || ""}
+          onChangeText={handleTimeChange}
+          onBlur={handleBlur}
+          keyboardType="numeric"
+        />
+      )}
+    </View>
+  );
 }
 
